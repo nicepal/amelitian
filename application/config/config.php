3070f2955126f45f124a73f5659ddeb0a56b5f8c
Asif Ahmed <asifahmed715@gmail.com>
2024-05-12 17:42:33 +0000

    update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   | a PHP script and you can easily do that on your own.
   |
  */
-$config['base_url'] = 'http://localhost:8888/amelitian/';
+$config['base_url'] = 'https://dev.amelitian.com/';
 
 
 /*

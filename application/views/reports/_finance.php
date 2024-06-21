f6584bc4d7f7da9c2bd9476eb8e12aa79bfe162f
Asif Ahmed <asifahmed715@gmail.com>
2024-06-21 23:44:17 +0500

    update

--- application/views/reports/_finance.php
+++ application/views/reports/_finance.php
@@ -18,7 +18,7 @@
                     if ($this->rbac->hasPrivilege('balance_fees_report', 'can_view')) {
                         ?>
 
-                        <li class="col-lg-4 col-md-4 col-sm-6 <?php echo set_SubSubmenu('Reports/finance/studentacademicreport'); ?>"><a href="<?php echo base_url(); ?>admin/transaction/studentacademicreport"><i class="fa fa-file-text-o"></i>
+                        <li class="col-lg-4 col-md-4 col-sm-6 <?php echo set_SubSubmenu('Reports/finance/studentacademicreport'); ?>"><a href="<?php echo base_url(); ?>admin/transaction/studentacademicreport_new"><i class="fa fa-file-text-o"></i>
                                 <?php echo $this->lang->line('balance_fees_report'); ?></a></li>
                         <?php
                     }

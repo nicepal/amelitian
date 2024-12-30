1bf5d4e522531e0824db4d15af876c6ee64592e1
Asif Ahmed <asifahmed715@gmail.com>
2024-12-30 21:47:48 +0500

    update

---
+++ application/controllers/admin/Followup.php
@@ -0,0 +1,321 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class Followup extends Admin_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->library('form_validation');
+        $this->load->model("followup_model");
+        $this->config->load("payroll");
+        $this->enquiry_status = $this->config->item('enquiry_status');
+    }
+
+    public function index() {
+
+
+        if (!$this->rbac->hasPrivilege('admission_enquiry', 'can_view')) {
+            access_denied();
+        }
+
+        $this->session->set_userdata('top_menu', 'front_office');
+        $this->session->set_userdata('sub_menu', 'admin/followup');
+        $data['class_list'] = $this->class_model->get();
+        $data['staffList']         = $this->staff_model->searchFullText("", 1);
+        $class                   =  $data['class_list'];
+        $data['classlist']       = $class;
+        $data["source_select"] = "";
+        $data["status"] = "active";
+       
+       
+             $this->form_validation->set_rules('from_date', $this->lang->line('followup')." ".$this->lang->line('from')." ".$this->lang->line('date'), 'trim|required|xss_clean');
+              $this->form_validation->set_rules('to_date', $this->lang->line('followup')." ".$this->lang->line('to')." ".$this->lang->line('date'), 'trim|required|xss_clean');
+
+        if ($this->form_validation->run() == TRUE) {
+          
+            $source = $this->input->post("source");
+            $status = $this->input->post("status");
+            $date_from = date("Y-m-d", $this->customlib->datetostrtotime($this->input->post("from_date")));
+            $date_to = date("Y-m-d", $this->customlib->datetostrtotime($this->input->post("to_date")));
+ 
+            $data["source_select"] = $source;
+            $data["status"] = $status;
+           
+            $enquiry_list = $this->followup_model->searchEnquiry($source, $status, $date_from, $date_to);
+            
+
+             
+           
+        } else {
+
+            $enquiry_list = $this->followup_model->getenquiry_list();
+        }
+
+        foreach ($enquiry_list as $key => $value) {
+
+            $follow_up = $this->followup_model->getFollowByEnquiry($value["id"]);
+
+            $enquiry_list[$key]["followupdate"] = $follow_up["date"]??'';
+            $enquiry_list[$key]["next_date"] = $follow_up["next_date"]??'';
+            $enquiry_list[$key]["response"] = $follow_up["response"]??'';
+            $enquiry_list[$key]["note"] = $follow_up["note"]??'';
+            $enquiry_list[$key]["followup_by"] = $follow_up["followup_by"]??'';
+        }
+        $data['enquiry_list'] = $enquiry_list;
+        $data['enquiry_status'] = $this->enquiry_status;
+        $data['Reference'] = $this->followup_model->get_reference();
+        $data['sourcelist'] = $this->followup_model->getComplaintSource();
+
+        $this->load->view('layout/header');
+        $this->load->view('admin/frontoffice/followupview', $data);
+        $this->load->view('layout/footer');
+    }
+
+    public function add() {
+        if (!$this->rbac->hasPrivilege('admission_enquiry', 'can_add')) {
+            access_denied();
+        }
+
+        $this->form_validation->set_rules('name', $this->lang->line('name'), 'trim|required|xss_clean');
+        $this->form_validation->set_rules('student_id', $this->lang->line('student'), 'trim|required|xss_clean');
+        $this->form_validation->set_rules('date', $this->lang->line('date'), 'trim|required|xss_clean');
+
+        if ($this->form_validation->run() == FALSE) {
+
+            $msg = array(
+                'name' => form_error('name'),
+                'contact' => form_error('contact'),
+                'student_id' => form_error('student_id'),
+                'date' => form_error('date'),
+            );
+
+            $array = array('status' => 'fail', 'error' => $msg, 'message' => '');
+        } else {
+            $enquiry = array(
+                'name' => $this->input->post('name'),
+                'contact' => $this->input->post('contact'),
+                'address' => $this->input->post('address'),
+                'reference' => $this->input->post('reference')??'-',
+                'date' => date('Y-m-d', $this->customlib->datetostrtotime($this->input->post('date'))),
+                'description' => $this->input->post('description'),
+                'follow_up_date' => date('Y-m-d', $this->customlib->datetostrtotime($this->input->post('follow_up_date'))),
+                'note' => $this->input->post('note'),
+                'source' => $this->input->post('source')??'-',
+                'email' => $this->input->post('email'),
+                'assigned' => $this->input->post('assigned'),
+                'adm_type' => $this->input->post('adm_type')??'-',
+                'class' => '0',
+                'no_of_child' => $this->input->post('no_of_child'),
+                'status' => 'active'
+            );
+            $enquiry_id = $this->followup_model->add($enquiry);
+
+            $student_name = $this->input->post("student_name");
+            $date_of_birth = $this->input->post("date_of_birth");
+            $class = $this->input->post("class");
+            $adhar_no = $this->input->post("adhar_no");
+            for($i =0; $i < count($student_name); $i++){
+                if($student_name[$i] != "" && $date_of_birth[$i] != "" && $class[$i] != ""){
+                    $info = array(
+                        'followup_id' => $enquiry_id,
+                        'name' => $student_name[$i],
+                        'dob' => $date_of_birth[$i],
+                        'class' => $class[$i],
+                        'adhar_no' => isset($adhar_no[$i])?($adhar_no[$i]):(''),
+                    );
+                    $this->db->insert("followup_students",$info);
+                }
+            }
+            $array = array('status' => 'success', 'error' => '', 'message' => $this->lang->line('success_message'));
+        }
+        echo json_encode($array);
+    }
+
+    public function delete($id) {
+        if (!$this->rbac->hasPrivilege('admission_enquiry', 'can_delete')) {
+            access_denied();
+        }
+        if (!empty($id)) {
+            $this->followup_model->enquiry_delete($id);
+            $this->followup_model->enquiry_students_delete($id);
+            $array = array('status' => 'success', 'error' => '', 'message' => $this->lang->line('delete_message'));
+        }
+        echo json_encode($array);
+    }
+
+    public function follow_up($enquiry_id, $status) {
+
+        if (!$this->rbac->hasPrivilege('follow_up_admission_enquiry', 'can_view')) {
+            access_denied();
+        }
+        $data['id'] = $enquiry_id;
+        $data['enquiry_data'] = $this->followup_model->getenquiry_list($enquiry_id, $status);
+        $data['next_date'] = $this->followup_model->next_follow_up_date($enquiry_id);
+        $data['staff'] = $this->staff_model->getProfile($data['enquiry_data']['assigned']);
+        $data['enquiry_status'] = $this->enquiry_status;
+        $data['enquiry_student'] = $this->followup_model->get_enquiry_student($enquiry_id);
+        $this->load->view('admin/frontoffice/followup_follow_up_modal', $data);
+    }
+
+    function follow_up_insert() {
+        if (!$this->rbac->hasPrivilege('follow_up_admission_enquiry', 'can_add')) {
+            access_denied();
+        }
+
+        $this->form_validation->set_rules('response', $this->lang->line('response'), 'trim|required|xss_clean');
+        $this->form_validation->set_rules('date', $this->lang->line('follow_up_date'), 'trim|required|xss_clean');
+        $this->form_validation->set_rules('follow_up_date', $this->lang->line('next_follow_up_date'), 'trim|required|xss_clean');
+
+        if ($this->form_validation->run() == FALSE) {
+
+            $msg = array(
+                'response' => form_error('response'),
+                'follow_up_date' => form_error('follow_up_date'),
+                'date' => form_error('date'),
+            );
+
+            $array = array('status' => 'fail', 'error' => $msg, 'message' => '');
+        } else {
+            $admin = $this->customlib->getLoggedInUserData();
+            $follow_up = array(
+                'date' => date('Y-m-d', $this->customlib->datetostrtotime($this->input->post('date'))),
+                'next_date' => date('Y-m-d', $this->customlib->datetostrtotime($this->input->post('follow_up_date'))),
+                'response' => $this->input->post('response'),
+                'note' => $this->input->post('note'),
+                'followup_by' => $admin['username'],
+                'followup_id' => $this->input->post('followup_id')
+            );
+            $this->followup_model->add_follow_up($follow_up);
+
+            $array = array('status' => 'success', 'error' => '', 'message' => $this->lang->line('success_message'));
+        }
+
+        echo json_encode($array);
+    }
+
+    function follow_up_list($id) {
+
+        $data['id'] = $id;
+        $data['follow_up_list'] = $this->followup_model->getfollow_up_list($id);
+        $this->load->view('admin/frontoffice/followuplist', $data);
+    }
+
+    function details($id, $status) {
+
+        if (!$this->rbac->hasPrivilege('admission_enquiry', 'can_view')) {
+            access_denied();
+        }
+
+        $data['source'] = $this->followup_model->getComplaintSource();
+        $data['enquiry_type'] = $this->followup_model->get_enquiry_type();
+        $data['Reference'] = $this->followup_model->get_reference();
+        $data['class_list'] = $this->followup_model->getclasses();
+        $data['classlist'] =  $data['class_list'];
+        $data['enquiry_data'] = $this->followup_model->getenquiry_list($id, $status);
+        $data['staffList']         = $this->staff_model->searchFullText("", 1);
+        $data['enquiry_student'] = $this->followup_model->get_enquiry_student($id);
+
+        $this->load->view('admin/frontoffice/followupeditmodalview', $data);
+    }
+
+    function editpost($id) {
+
+        if (!$this->rbac->hasPrivilege('admission_enquiry', 'can_edit')) {
+            access_denied();
+        }
+
+        $this->form_validation->set_rules('name', $this->lang->line('name'), 'trim|required|xss_clean');
+        $this->form_validation->set_rules('contact', $this->lang->line('contact'), 'trim|required|xss_clean');
+        $this->form_validation->set_rules('source', $this->lang->line('source'), 'trim|required|xss_clean');
+        $this->form_validation->set_rules('date', $this->lang->line('date'), 'trim|required|xss_clean');
+
+        if ($this->form_validation->run() == FALSE) {
+
+            $msg = array(
+                'name' => form_error('name'),
+                'contact' => form_error('contact'),
+                'source' => form_error('source'),
+                'date' => form_error('date'),
+            );
+
+            $array = array('status' => 'fail', 'error' => $msg, 'message' => '');
+        } else {
+            $enquiry_update = array(
+                'name' => $this->input->post('name'),
+                'contact' => $this->input->post('contact'),
+                'address' => $this->input->post('address'),
+                'reference' => $this->input->post('reference'),
+                'date' => date('Y-m-d', $this->customlib->datetostrtotime($this->input->post('date'))),
+                'description' => $this->input->post('description'),
+                'follow_up_date' => date('Y-m-d', $this->customlib->datetostrtotime($this->input->post('follow_up_date'))),
+                'note' => $this->input->post('note'),
+                'source' => $this->input->post('source'),
+                'email' => $this->input->post('email'),
+                'assigned' => $this->input->post('assigned'),
+                'class' => '0',
+                'no_of_child' => $this->input->post('no_of_child'),
+                'adm_type' => $this->input->post('adm_type'),
+
+            );
+            $this->followup_model->enquiry_update($id, $enquiry_update);
+
+            $this->followup_model->enquiry_students_delete($id);
+            $student_name = $this->input->post("student_name");
+            $date_of_birth = $this->input->post("date_of_birth");
+            $class = $this->input->post("class");
+            $adhar_no = $this->input->post("adhar_no");
+            for($i =0; $i < count($student_name); $i++){
+                if($student_name[$i] != "" && $date_of_birth[$i] != "" && $class[$i] != ""){
+                    $info = array(
+                        'followup_id' => $id,
+                        'name' => $student_name[$i],
+                        'dob' => $date_of_birth[$i],
+                        'class' => $class[$i],
+                        'adhar_no' => isset($adhar_no[$i])?($adhar_no[$i]):(''),
+                    );
+                    $this->db->insert("followup_students",$info);
+                }
+            }
+            
+            $array = array('status' => 'success', 'error' => '', 'message' => $this->lang->line('update_message'));
+        }
+
+        echo json_encode($array);
+    }
+
+    public function follow_up_delete($follow_up_id, $enquiry_id) {
+        if (!$this->rbac->hasPrivilege('follow_up_admission_enquiry', 'can_delete')) {
+            access_denied();
+        }
+        $this->followup_model->delete_follow_up($follow_up_id);
+        $data['id'] = $enquiry_id;
+        $data['follow_up_list'] = $this->followup_model->getfollow_up_list($enquiry_id);
+
+        $this->load->view('admin/frontoffice/followuplist', $data);
+    }
+
+    public function check_default($post_string) {
+        return $post_string == '' ? FALSE : TRUE;
+    }
+
+    public function change_status() {
+
+        $id = $this->input->post("id");
+        $status = $this->input->post("status");
+
+        if (!empty($id)) {
+            $data = array('id' => $id, 'status' => $status);
+
+            $this->followup_model->changeStatus($data);
+            $array = array('status' => 'success', 'error' => '', 'message' => $this->lang->line('success_message'));
+        } else {
+
+            $array = array('status' => 'fail', 'error' => '', 'message' => $this->lang->line('update_message'));
+        }
+
+        echo json_encode($array);
+    }
+
+}
